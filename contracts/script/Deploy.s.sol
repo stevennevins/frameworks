6722968f8aa547566ef63fb94e238ddd2bff5a1e
steven nevins <steven.nevins.js@gmail.com>
2024-03-23 14:53:56 -0400

    update deploy script

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {NFT} from "../src/NFT.sol";
+import {RankedAuction} from "../src/RankedAuction.sol";
 
 contract Deploy is Script {
 
     function run() public {
-        vm.broadcast();
-        new NFT("Nouns Collective", "Nouns");
+        vm.startBroadcast();
+        NFT nft = new NFT("Nouns Collective", "Nouns");
+        RankedAuction auction = new RankedAuction(address(nft), 100, block.timestamp, block.timestamp + 100, 0.001 ether);
+        nft.updateMinter(address(auction));
+        vm.stopBroadcast();
 
     }
 }
--- contracts/src/NFT.sol
+++ contracts/src/NFT.sol
@@ -4,16 +4,16 @@ pragma solidity ^0.8.0;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract NFT is ERC721{
     uint256 public nextTokenId;
-    mapping(address=>bool) public isMinter;
+    address public minter;
     constructor(string memory name, string memory symbol) ERC721(name, symbol){}
 
     modifier onlyMinter() {
-        require(isMinter[msg.sender], "Not Minter");
+        require(msg.sender == minter, "Not Minter");
         _;
     }
 
-    function addMinter(address minter) public {
-        isMinter[minter] = true;
+    function updateMinter(address _minter) public {
+        minter = _minter;
     }
 
     function mint(address to, uint256 amount) external {
